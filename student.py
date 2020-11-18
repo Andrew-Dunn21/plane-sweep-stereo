ca8e6c6efbc5a442a77e25583d8ad31a311ac6d0
Andrew-Dunn21 <dunna21@wwu.edu>
2020-11-17 16:40:01 -0800

    Fixed project/unproject

--- student.py
+++ student.py
@@ -15,21 +15,33 @@ def project_impl(K, Rt, points):
         projections -- height x width x 2 array of 2D projections
     """
     
-    #I'm trying to think of a way to vectorize this for efficiency
-    #LMK if you have any ideas
-    h,w = points.shape[:2]
-    #The output array
-    outs = np.zeros((h,w,2))
-    #Loop the points
-    for i in range(h):
-        for j in range(w):
-            x,y,z = points[i,j]
-            u,v,t = np.dot(K,np.dot(Rt,np.array([[x],[y],[z],[1]])))
-            if t != 0:
-                u = u/t
-                v = v/t
-            outs[i,j,:] = (u,v)
-    return outs
+    h,w,d = points.shape
+    if d != 3:
+        raise Exception("These points don't add up...")
+    #Set up an output array
+    proj = np.zeros((1,h*w, 2), dtype=np.float32)
+    #Copy points for safety
+    pints = np.copy(points)
+    #Smash the pints for easy peasy looping
+    pints = pints.reshape((1, h*w,d))
+    #All we do is ride that loop
+    for i in range(h*w):
+        #Get the 3D point from pints
+        x,y,z = pints[0,i,:]
+        v = np.array([[x],[y],[z],[1]], dtype=np.float32)
+        camv = np.dot(Rt,v)
+        if camv[2,0] != 0:
+            camv /= camv[2,0]
+        else:
+            raise Exception("This is sus behavior")
+        pixv = np.dot(K,camv)
+        if pixv[2,0] != 0:
+            pixv /= pixv[2,0]
+        else:
+            raise Exception("Just full of sus today")
+        proj[0,i,:] = pixv[:2,0]
+    #Reshape and return proj
+    return proj.reshape((h,w,2))
 
 
 def unproject_corners_impl(K, width, height, depth, Rt):
@@ -45,66 +57,56 @@ def unproject_corners_impl(K, width, height, depth, Rt):
     Output:
         points -- 2 x 2 x 3 array of 3D positions of the image corners
     """
-
-    #Things we need to deproject:
+    #Pick up the pieces to unproject
     Ki = np.linalg.inv(K)
-    Ri = np.eye(4)
-    Ri[:3,:3] = Rt[:,:3]
-    Ri = np.transpose(Ri)
-    Ri2 = np.linalg.inv(Ri)
-    
-    #Doing this carefully because prior errors
-    Ti = np.eye(4)
-    T = np.ones((4,1))
-    T[:3,0] = Rt[:,3]
-    T = np.dot(Ri,T)
-    Ti[:,3] = T[:,0]
-    Tinv = np.linalg.inv(Ti)
-    P = np.zeros((4,3))
-    P[:3,:] = np.eye(3)
-    P[3,2] = 1
-    
-    #Designate the corners as np arrays
+    Ri = np.eye(4,dtype=np.float32)
+    Ri[:3,:] = Rt
+    Ri = np.linalg.inv(Ri)
+    P = np.zeros((4,3), dtype=np.float32)
+    P[:3,:] = np.eye(3, dtype=np.float32)
+    #Because I'm lazy
+    h = height-1
+    w = width-1
+    #Make an output
+    points = np.zeros((2,2,3), dtype=np.float32)
+    #Establish corners
     bL = np.array([
         [0],
         [0],
         [1]],dtype=np.float32)
     bR = np.array([
-        [width-1],
+        [w],
         [0],
         [1]],dtype=np.float32)
     tL = np.array([
         [0],
-        [height-1],
+        [h],
         [1]],dtype=np.float32)
     tR = np.array([
-        [width-1],
-        [height-1],
+        [w],
+        [h],
         [1]],dtype=np.float32)
+    #No loop, directly handle
+    bL = np.dot(P, depth*np.dot(Ki, bL))
+    bL[3,0] = 1
+    bL = np.dot(Ri, bL)
+    bR = np.dot(P, depth*np.dot(Ki, bR))
+    bR[3,0] = 1
+    bR = np.dot(Ri, bR)
+    tL = np.dot(P, depth*np.dot(Ki, tL))
+    tL[3,0] = 1
+    tL = np.dot(Ri, tL)
+    tR = np.dot(P, depth*np.dot(Ki, tR))
+    tR[3,0] = 1
+    tR = np.dot(Ri, tR)
     
-    #Deproject factoring in depth
-    bL = np.dot(Tinv, np.dot(Ri, np.dot(P, depth*np.dot(Ki,bL))))
-    bR = np.dot(Tinv, np.dot(Ri, np.dot(P, depth*np.dot(Ki,bR))))
-    tL = np.dot(Tinv, np.dot(Ri, np.dot(P, depth*np.dot(Ki,tL))))
-    tR = np.dot(Tinv, np.dot(Ri, np.dot(P, depth*np.dot(Ki,tR))))
-    
-    #Norm if necessary, we should never get 0, though
-    if bL[3,0] != 1:
-        w = bL[3,0]
-        bL /= w
-        bR /= w
-        tL /= w
-        tR /= w
-    
-    #Put the corners into an output array
-    points = np.zeros((2,2,3),dtype=np.float32)
+    #Manually set output
     points[0,0,:] = bL[:3,0]
     points[0,1,:] = bR[:3,0]
     points[1,0,:] = tL[:3,0]
     points[1,1,:] = tR[:3,0]
     
     return points
-
 
 def preprocess_ncc_impl(image, ncc_size):
     """
@@ -194,6 +196,7 @@ def preprocess_ncc_impl(image, ncc_size):
 
     # compute vector norm and divide each patch by it if norm is non-zero
     v_norm = np.linalg.norm(new_norm, axis=2)
+    v_norm[v_norm < 1e-6] = 0
     new_norm[v_norm > 0] = new_norm[v_norm > 0] / v_norm[v_norm > 0, np.newaxis]
 
     return new_norm
@@ -214,7 +217,6 @@ def compute_ncc_impl(image1, image2):
 
     h, w, p = image1.shape
 
-    # create ncc array
     ncc = np.zeros((h, w))
 
     # compute the dot product for each patch and store in ncc array
