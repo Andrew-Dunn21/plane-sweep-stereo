db444eaba10fe2ac85842bd988067e86aad79ab5
Andrew-Dunn21 <dunna21@wwu.edu>
2021-03-27 14:00:43 -0700

    Renaming

--- workhorse.py
+++ workhorse.py
@@ -118,38 +118,6 @@ def preprocess_ncc_impl(image, ncc_size):
     vectors and then 'compute_ncc' is called to compute the dot product
     between these vectors in two images.
 
-    NCC preprocessing has two steps.
-    (1) Compute and subtract the mean.
-    (2) Normalize the vector.
-
-    The mean is per channel.  i.e. For an RGB image, over the ncc_size**2
-    patch, compute the R, G, and B means separately.  The normalization
-    is over all channels.  i.e. For an RGB image, after subtracting out the
-    RGB mean, compute the norm over the entire (ncc_size**2 * channels)
-    vector and divide.
-
-    If the norm of the vector is < 1e-6, then set the entire vector for that
-    patch to zero.
-
-    Patches that extend past the boundary of the input image at all should be
-    considered zero.  Their entire vector should be set to 0.
-
-    Patches are flattened into vectors with the default numpy row
-    major order.  For example, the following 3D numpy array with shape
-    2 (channels) x 2 (height) x 2 (width) patch...
-
-    channel1         channel2
-    +------+------+  +------+------+ height
-    | x111 | x121 |  | x112 | x122 |  |
-    +------+------+  +------+------+  |
-    | x211 | x221 |  | x212 | x222 |  |
-    +------+------+  +------+------+  v
-    width ------->
-
-    gets unrolled using np.reshape into a vector in the following order:
-
-    v = [ x111, x121, x211, x221, x112, x122, x212, x222 ]
-
     Input:
         image -- height x width x channels image of type float32
         ncc_size -- integer side length of (square) NCC patch region; must be odd
@@ -172,20 +140,7 @@ def preprocess_ncc_impl(image, ncc_size):
             # so the (i=0,j=0) is the pixel in the top-left corner of the patch
             # which is an offset of (-k, -k) from the center pixel
 
-            # example: image is 10x10x3; ncc_size is 3
-            # the image pixels for the top left of all patches come from
-            # (0, 0) thru (7, 7) because (7, 7) is an offset of -1, -1 
-            # (corresponding to i=0, j=0) from the bottom-right-most patch, 
-            # which is centered at (8, 8)
-            # generalizing to patch halfsize k, it's (h-2k, w-2k)
-            # generalizing to any offset into the patch,
-            #   the top left will be i, j
-            #   the bottom right will be h-2k + i, w-2k + j
             normalized[k:h-k, k:w-k, :, i, j] = image[i:h-2*k+i, j:w-2*k+j, :]
-
-    # For each patch, subtract out its per-channel mean
-    # Then divide the patch by its (not-per-channel) vector norm.
-    # Patches with norm < 1e-6 should become all zeros.
 
     # compute and subtract per-channel mean
     means = np.mean(normalized, axis=(3, 4))
